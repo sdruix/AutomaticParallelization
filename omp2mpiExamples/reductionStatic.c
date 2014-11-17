66f6c7ca0c7de76786e05a353cf3e8552fcc5e36
Albert Saa-Garriga <albert.saa@uab.cat>
2014-11-17 15:55:37 +0100

    Omp2mpi Input Code Examples

---
+++ omp2mpiExamples/reductionStatic.c
@@ -0,0 +1,18 @@
+#include "mpi.h"
+#include <stdio.h>
+#define N 10
+
+int main()
+{
+	double sum=0, pi;
+	double step = 1.0/(double) N;
+#pragma omp parallel for static reduction (+:sum) check
+	for(int i = 0; i<N; ++i) {
+		double x = (i+0.5) * step;
+		sum = sum + 4.0/(1.0+x+x);
+	}
+	pi = step*sum;
+	printf("PI: %f\n", pi);	
+	return pi;
+}
+
